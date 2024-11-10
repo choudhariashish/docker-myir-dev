97048bca95bb7f1c5d4cc5b42270c2fef80c50f3
ashish <choudhariashish@gmail.com>
2024-11-09 16:47:53 -0800

    update script to setup m4 build env

--- create-image.sh
+++ create-image.sh
@@ -2,20 +2,34 @@
 
 if [ ! -f downloads/04-Source.tar.bz2 ]; then
     wget https://d.myirtech.com/MYS-8MMX/L5.4.3/04-Source.tar.bz2
-    tar -xf 04-Source.tar.bz2
     mkdir downloads
     mv 04-Source.tar.bz2 downloads/
 fi
 
 
 if [ ! -f share/04-Source/mys-8mmx-yocto.tar.gz ]; then
-    mv 04-Source/ share/
+
+    echo "Extracting SDKs..."
+    tar -jxf downloads/04-Source.tar.bz2 -C share/
 
     cd share/04-Source/
     tar xf mys-8mmx-yocto.tar.gz
+    tar -xvzf m4.tar.gz
+
+    echo "Setup M4 build tools/SDK..."
+    cd m4/
+    mkdir toolchains
+    tar -jxf gcc-arm-none-eabi-7-2017-q4-major-linux.tar.bz2 -C toolchains/
+    mkdir SDK_2.8.0_EVK-MIMX8MM
+    tar -zxf SDK_2.8.0_EVK-MIMX8MM.tar.gz -C SDK_2.8.0_EVK-MIMX8MM/
+    cp ../../scripts/setup_m4_env.sh .
+    cd ../
+
+    echo "Setup Linux build tools/SDK..."
     cp -r ../meta-pluto imx-yocto-bsp/sources/meta-pluto
     cd imx-yocto-bsp/
 
+    echo "Prepare build scipt for yocto..."
     echo "#!/bin/bash" >> core.sh
     echo "cp -r ../../meta-pluto sources/" >> core.sh
     echo "DISTRO=fsl-imx-xwayland MACHINE=mys-8mmx source sources/meta-pluto/tools/pluto-setup-release.sh -b build-xwayland" >> core.sh
@@ -25,7 +39,7 @@ if [ ! -f share/04-Source/mys-8mmx-yocto.tar.gz ]; then
 
     cd ../../../
 
-    # Patch the sources.
+    echo "Patch the sources..."
     ./apply-patch.sh patches/pluto-patch-1.diff share/04-Source/imx-yocto-bsp/sources/
     ./apply-patch.sh patches/pluto-patch-2.diff share/04-Source/imx-yocto-bsp/sources/
     chmod -R 777 share/
---
+++ share/scripts/setup_m4_env.sh
@@ -0,0 +1,2 @@
+export ARMGCC_DIR=`pwd`/toolchains/gcc-arm-none-eabi-7-2017-q4-major
+env | grep ARMGCC_DIR
