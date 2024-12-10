d981348c32a59751152b60dd651a13fdccaf265e
ashish <choudhariashish@gmail.com>
2024-12-09 16:55:09 -0800

    pull boot scr in fetch script

--- out/fetch-images.sh
+++ out/fetch-images.sh
@@ -1,5 +1,6 @@
 cp ../share/04-Source/imx-yocto-bsp/build-xwayland/tmp/deploy/images/mys-8mmx/imx-boot .
 cp ../share/04-Source/imx-yocto-bsp/build-xwayland/tmp/deploy/images/mys-8mmx/fitimage.fit .
+cp ../share/scripts/boot_type2.scr boot.scr
 cp fitimage.fit /tftpboot
 
 printf "\n\nDo the following on uboot cli...\n\n"
