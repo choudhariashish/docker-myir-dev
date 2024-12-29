2a57c6446e9d84e9f9d850025f62b39692d3c82e
ashish <choudhariashish@gmail.com>
2024-12-29 13:07:27 -0800

    move pluto recipes to pluto layer

--- share/rt-app/boards/evkmimx8mm/demo_apps/hello_world/hello_world.c
+++ share/rt-app/boards/evkmimx8mm/demo_apps/hello_world/hello_world.c
@@ -49,9 +49,6 @@ int main(void)
 
     while (1)
     {
-//        ch = GETCHAR();
-//        PUTCHAR(ch);
-
         if (0x01 == ipcObj->aTobMsg.ready)
         {
             PRINTF("rx:%x %x %x %x\r\n", ipcObj->aTobMsg.payload[0],
@@ -71,77 +68,3 @@ int main(void)
         }
     }
 }
-
-/*
-#define IPC_NODE_A     1
-#define IPC_NODE_B     2
-#define IPC_NODE_ID    IPC_NODE_B
-
-#define IPC_MSG_PAYLOAD_SIZE    1024
-#define IPC_MSG_QSIZE           10
-
-struct Msg
-{
-    uint32_t id;
-    char payload[IPC_MSG_PAYLOAD_SIZE];
-    uint32_t crc;
-};
-typedef struct Msg Msg_t;
-
-struct IPCMsgQueue
-{
-    int count;
-    int head;
-    int tail;
-    Msg_t msgs[IPC_MSG_QSIZE];
-};
-typedef struct IPCMsgQueue IPCMsgQueue_t;
-
-struct IPCMsg
-{
-    IPCMsgQueue_t aTobQ;
-    IPCMsgQueue_t bToaQ;
-};
-typedef struct IPCMsg IPCMsg_t;
-
-void IPCMsgInitQ(IPCMsgQueue_t *q)
-{
-    q->count = 0;
-    q->head = 0;
-    q->tail = 0;
-}
-
-int IPCMsgSend(IPCMsgQueue_t *q, Msg_t *m)
-{
-    // Q full case.
-    if (q->head == q->tail && q->count != 0)
-    {
-        return 1;
-    }
-    q->head = (q->head + 1) % IPC_MSG_QSIZE;
-    memcpy(&q->msgs[q->head], m, sizeof(Msg_t));
-    q->count++;
-    return 0;
-}
-
-int IPCMsgGet(IPCMsgQueue_t *q, Msg_t *m)
-{
-    if (NULL == m)
-    {
-        return 1;
-    }
-
-    // Q empty case.
-    if (q->tail == q->head && q->count == 0)
-    {
-        return 1;
-    }
-
-    q->tail = (q->tail + 1) % IPC_MSG_QSIZE;
-    memcpy(m, &q->msgs[q->tail], sizeof(Msg_t));
-    q->count--;
-    return 0;
-}
-
-IPCMsg_t *ipcMsg = (IPCMsg_t *)0x80100000;
-*/
