1a855dd22e85c6c2cd7bdaa96dff034c26fbc4eb
ashish <choudhariashish@gmail.com>
2024-12-29 22:00:03 -0800

    remove build files

--- share/pluto-mcu/apps/pluto-rt/armgcc/CMakeFiles/pluto-rt.elf.dir/home/ashish/dev/docker-myir-dev/share/04-Source/m4/SDK_2.8.0_EVK-MIMX8MM/devices/MIMX8MM6/utilities/fsl_sbrk.c.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/pluto-rt.elf.dir/home/ashish/dev/docker-myir-dev/share/04-Source/m4/SDK_2.8.0_EVK-MIMX8MM/devices/MIMX8MM6/utilities/fsl_sbrk.c.obj: \
- /home/ashish/dev/docker-myir-dev/share/04-Source/m4/SDK_2.8.0_EVK-MIMX8MM/devices/MIMX8MM6/utilities/fsl_sbrk.c
--- share/pluto-mcu/apps/pluto-rt/armgcc/CMakeFiles/pluto-rt.elf.dir/home/ashish/dev/docker-myir-dev/share/pluto-mcu/apps/pluto-rt/empty_rsc_table.c.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/pluto-rt.elf.dir/home/ashish/dev/docker-myir-dev/share/pluto-mcu/apps/pluto-rt/empty_rsc_table.c.obj: \
- /home/ashish/dev/docker-myir-dev/share/pluto-mcu/apps/pluto-rt/empty_rsc_table.c
